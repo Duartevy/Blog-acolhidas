203932855315c610a825703018f5ff30da4e7892
Duartevy <milenegba@gmail.com>
2024-12-18 20:09:16 -0300

    Adiciona melhorias no blog

--- public/script.js
+++ public/script.js
@@ -1,1 +1,5 @@
- 
+ // Mensagem de boas-vindas
+document.addEventListener("DOMContentLoaded", function () {
+    alert("Bem-vinda ao Blog Acolhidas! Compartilhe suas histórias com a gente.");
+});
+
